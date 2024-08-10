0d0f033771bd61a0105699665bae1a3ba72050a1
Happy-clo <support@wdsj.one>
2024-08-10 22:15:25 +0800

    "add: efficiency command for ViaVersion plugin"
     ViaVersion插件已更新，新增效率模式命令。此更新包括对build.yml的更改，用于上传构建工件，并在ViaVersionPlugin中添加了对/efficiency命令的支持。此外，还进行了代码重构，以整合和优化Bukkit命令处理。

--- bukkit/src/main/java/com/viaversion/viaversion/bukkit/commands/BukkitCommandHandler.java
+++ bukkit/src/main/java/com/viaversion/viaversion/bukkit/commands/BukkitCommandHandler.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of ViaVersion - https://github.com/ViaVersion/ViaVersion
- * Copyright (C) 2016-2024 ViaVersion and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.viaversion.viaversion.bukkit.commands;
 
 import com.viaversion.viaversion.commands.ViaCommandHandler;
@@ -24,14 +7,39 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
+/**
+ * Bukkit命令处理器类，扩展了ViaCommandHandler，实现了CommandExecutor和TabExecutor接口，
+ * 用于处理Bukkit插件中的命令和标签完成.
+ */
 public class BukkitCommandHandler extends ViaCommandHandler implements CommandExecutor, TabExecutor {
+
+    /**
+     * 处理命令执行的方法.
+     * 
+     * @param sender 命令发送者
+     * @param command 被执行的命令
+     * @param label 命令标签
+     * @param args 命令参数
+     * @return 如果命令被成功处理则返回true，否则返回false
+     */
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // 将命令发送者转换为BukkitCommandSender对象，然后调用父类的onCommand方法进行处理
         return onCommand(new BukkitCommandSender(sender), args);
     }
 
+    /**
+     * 处理命令的标签完成方法.
+     * 
+     * @param sender 命令发送者
+     * @param command 被执行的命令
+     * @param alias 命令别名
+     * @param args 命令参数
+     * @return 返回一个包含可能的命令完成选项的列表
+     */
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        // 将命令发送者转换为BukkitCommandSender对象，然后调用父类的onTabComplete方法进行处理
         return onTabComplete(new BukkitCommandSender(sender), args);
     }
 }
