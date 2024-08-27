827d8c9c5d76a186024c7ad152047ec29e091f79
Happy <admin@maoyuan.us>
2024-08-27 17:24:47 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     base
     id("via.build-logic")
-    id("com.github.core-lib.xjar") version "4.0.2"
 }
 
 allprojects {
     group = "com.viaversion"
-    version = property("projectVersion") as String // 从 gradle.properties 中获取项目版本
+    version = property("projectVersion") as String // from gradle.properties
     description = "Allows the connection of newer clients to older server versions for Minecraft servers."
 }
 
-val mainProjects = setOf(
+val main = setOf(
     projects.viaversion,
     projects.viaversionCommon,
     projects.viaversionApi,
@@ -21,28 +20,7 @@ val mainProjects = setOf(
 
 subprojects {
     when (this) {
-        in mainProjects -> plugins.apply("via.shadow-conventions")
+        in main -> plugins.apply("via.shadow-conventions")
         else -> plugins.apply("via.base-conventions")
     }
-}
-
-// 注册 XJar 任务
-tasks.register<com.github.core_lib.xjar.XJarTask>("xjar") {
-    // XJar 的配置
-    password.set("io.xjar")
-
-    // 可选参数
-    keySize.set(128) // 设置密钥大小
-    ivSize.set(16) // 设置初始化向量大小
-    includes.add("**/*.class") // 添加需要包含的文件
-    excludes.add("**/api/**") // 添加需要排除的文件
-    sourceDir.set(file("build/libs")) 
-    sourceJar.set(file("build/libs/ViaVersion-5.0.4-SNAPSHOT.jar"))
-    targetDir.set(file("build/libs"))
-    targetJar.set(file("yourTarget.jar"))
-}
-
-// 使 jar 任务依赖于 xjar 任务
-tasks.named("jar") {
-    dependsOn("xjar")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,22 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    // 配置所有项目的仓库
+    // configures repositories for all projects
     repositories {
-        mavenCentral()
         maven("https://repo.viaversion.com")
-        maven("https://jitpack.io")
         maven("https://repo.papermc.io/repository/maven-public/")
+        mavenCentral()
     }
+    // only use these repos
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
 
 pluginManagement {
-    // 默认插件版本
-    repositories {
-        maven("https://jitpack.io") // 如果插件在 JitPack 上
-    }
-    
+    // default plugin versions
     plugins {
-        id("com.github.core-lib.xjar") version "4.0.2"
         id("net.kyori.blossom") version "2.1.0"
         id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
         id("com.gradleup.shadow") version "8.3.0"
-        id("com.github.core-lib.xjar") version "4.0.2" // XJar 插件
     }
 }
 
@@ -30,7 +24,6 @@ rootProject.name = "viaversion-parent"
 
 includeBuild("build-logic")
 
-// 设置子项目
 setupViaSubproject("api")
 setupViaSubproject("common")
 setupViaSubproject("bukkit")
