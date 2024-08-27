2bbdce1e5db55ca87bfe2334cc9bedd400d58732
Happy <admin@maoyuan.us>
2024-08-27 16:45:11 +0800

    refactor(settings.gradle.kts): 更新仓库和插件配置
    - 注释使用中文，明确配置含义。
    - 精简仓库配置，确保一致性。
    - 添加缺失的子项目设置方法调用。
    - 引入XJar插件，助力子模块管理。

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,29 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 dependencyResolutionManagement {
-    // configures repositories for all projects
+    // 配置所有项目的仓库
     repositories {
         maven("https://repo.viaversion.com")
         maven("https://jitpack.io")
         maven("https://repo.papermc.io/repository/maven-public/")
         mavenCentral()
     }
-    // only use these repos
+    // 只使用这些仓库
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenCentral()
-        maven {
-            url = uri("https://mvnrepository.com/artifact/")
-        }
-    }
 }
 
 pluginManagement {
-    // default plugin versions
+    // 默认插件版本
+    repositories {
+        gradlePluginPortal()
+        maven("https://jitpack.io") // 如果插件在 JitPack 上
+    }
+    
     plugins {
         id("net.kyori.blossom") version "2.1.0"
         id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
         id("com.gradleup.shadow") version "8.3.0"
+        id("com.github.core-lib.xjar") version "4.0.2" // XJar 插件
     }
 }
 
@@ -31,6 +31,7 @@ rootProject.name = "viaversion-parent"
 
 includeBuild("build-logic")
 
+// 设置子项目
 setupViaSubproject("api")
 setupViaSubproject("common")
 setupViaSubproject("bukkit")
