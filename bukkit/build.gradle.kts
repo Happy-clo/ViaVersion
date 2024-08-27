ea69c97b01c942da3f1f1cebc555beab097bf525
Happy <admin@maoyuan.us>
2024-08-27 17:34:20 +0800

    fix

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -1,9 +1,10 @@
 dependencies {
+    compileOnly("net.luckperms:api:5.4")
     implementation(projects.viaversionBukkitLegacy)
     compileOnlyApi(projects.viaversionCommon)
-    
     compileOnly(libs.paper) {
         exclude("junit", "junit")
         exclude("com.google.code.gson", "gson")
         exclude("javax.persistence", "persistence-api")
+    }
 }
