18712a616d3d11bd343e91e4fb769849560f0c3d
Happy <admin@maoyuan.us>
2024-08-27 16:28:23 +0800

    fix

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     }
 
     // 添加 XJar 依赖
-    implementation 'com.github.core-lib:xjar:4.0.2'
+    implementation("com.github.core-lib:xjar:4.0.2")
     
     // 如果使用 JUnit 测试类来运行加密，可以将 XJar 依赖的 scope 设置为 test
     // testImplementation 'com.github.core-lib:xjar:4.0.2'
