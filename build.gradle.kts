47e3583be9dc5186464e726ee928ec92c6854378
Happy <admin@maoyuan.us>
2024-08-27 16:42:04 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     base
     id("via.build-logic")
+    id("com.github.core-lib.xjar") version "4.0.2" apply false
 }
 
 allprojects {
     group = "com.viaversion"
-    version = property("projectVersion") as String // from gradle.properties
+    version = property("projectVersion") as String // 从 gradle.properties 中获取项目版本
     description = "Allows the connection of newer clients to older server versions for Minecraft servers."
 }
 
-val main = setOf(
+val mainProjects = setOf(
     projects.viaversion,
     projects.viaversionCommon,
     projects.viaversionApi,
@@ -20,26 +21,23 @@ val main = setOf(
 
 subprojects {
     when (this) {
-        in main -> plugins.apply("via.shadow-conventions")
+        in mainProjects -> plugins.apply("via.shadow-conventions")
         else -> plugins.apply("via.base-conventions")
     }
 }
+
 repositories {
     maven("https://jitpack.io")
 }
 
-plugins {
-    id("com.github.core-lib.xjar") version "4.0.2" apply false
-}
-
+// 注册 XJar 任务
 tasks.register<com.github.core_lib.xjar.XJarTask>("xjar") {
-    // 这里是 XJar 的配置
+    // XJar 的配置
     password.set("io.xjar")
 
     // 可选参数
-    // algorithm.set("yourAlgorithm")
-    keySize.set(128)
-    ivSize.set(16)
+    keySize.set(128) // 设置密钥大小
+    ivSize.set(16) // 设置初始化向量大小
     includes.add("**/*.class") // 添加需要包含的文件
     excludes.add("**/api/**") // 添加需要排除的文件
     sourceDir.set(file("build/libs")) 
@@ -48,6 +46,7 @@ tasks.register<com.github.core_lib.xjar.XJarTask>("xjar") {
     targetJar.set(file("yourTarget.jar"))
 }
 
+// 使 jar 任务依赖于 xjar 任务
 tasks.named("jar") {
     dependsOn("xjar")
 }
